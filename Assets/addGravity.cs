ff19b22856d4f78e34890f09a4828996f1eb246b
Joseph Zamora <joseph.zamora64@yahoo.com>
2018-07-07 22:36:13 -0600

    Destruccion del planeta, sonidos del planeta y nuevo mesh del planeta.

---
+++ Assets/addGravity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class addGravity : MonoBehaviour {
+
+	#region Variables
+	[SerializeField]
+	[Range(0.1f,2f)]
+	private float mass;
+    #endregion
+
+    #region Unity Methods
+
+    void Start () {
+		mass = 1f;
+		gameObject.AddComponent<Rigidbody>();
+		gameObject.GetComponent<Rigidbody>().mass = mass;
+    }
+
+	void Update()
+	{
+		int x = (int) Random.RandomRange(0, 3);
+		if (x == 0)
+			gameObject.GetComponent<Rigidbody>().AddForce(Vector3.left * 2);
+		else if(x == 1)
+			gameObject.GetComponent<Rigidbody>().AddForce(Vector3.right * 2);
+		else if (x == 2)
+			gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 2);
+		else
+			gameObject.GetComponent<Rigidbody>().AddForce(Vector3.back * 2);
+
+	}
+
+    #endregion
+}
---
+++ Assets/despawnFractured.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class despawnFractured : MonoBehaviour {
+
+    #region Variables
+	[SerializeField]
+	private float despawnTime;
+	private float aux;
+	[SerializeField]
+	private AudioSource crackling;
+	#endregion
+
+	#region Unity Methods
+
+	void Start () {
+		crackling.Play();
+		aux = despawnTime;
+    }
+    
+    void Update () {
+		despawnTime -= Time.deltaTime;
+        if(despawnTime < 0)
+		{
+			Destroy(gameObject);
+		}
+    }
+
+    #endregion
+}
