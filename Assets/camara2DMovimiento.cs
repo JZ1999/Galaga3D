9fba64fecda47a7a5c5eebe5b928ce4405ef7657
Joseph Zamora <joseph.zamora64@yahoo.com>
2018-07-27 20:56:15 -0600

    Agregando movimiento de camara2D

--- Assets/camara2DMovimiento.cs
+++ Assets/camara2DMovimiento.cs
@@ -1,9 +1,7 @@
 ﻿using UnityEngine;
 
-//TODO:
-//Mover de camara 2D a 3D mediante una animación
 [DisallowMultipleComponent]
-public class Camara3D : MonoBehaviour {
+public class camara2DMovimiento : MonoBehaviour {
 
 	#region Variables
 	[SerializeField]
@@ -15,40 +13,37 @@ public class Camara3D : MonoBehaviour {
 	private Vector3 offset;
 
 	static public float tiempo;//Que tenga un tiempo dado antes de que empiece
-						//a llamar la funcion conseguirJugadorTR() debido a
-						//que sino causa un error de objeto null.
+							   //a llamar la funcion conseguirJugadorTR() debido a
+							   //que sino causa un error de objeto null.
 	#endregion
 
-	#region Unity Methods
-
-	private void Awake()
-	{
+	#region Metodos de Unity
+	void Start () {
 		tiempo = 2f;
 	}
-
-	private void LateUpdate()
-	{
+    
+    void Update () {
 		tiempo -= Time.deltaTime;
-		if(tiempo <= 0)
+		if (tiempo <= 0)
 		{
 			conseguirJugadorTR();
 		}
-		
-		
 	}
 
-	void FixedUpdate () {
+	void FixedUpdate()
+	{
 		//Este codigo hace un efecto natural de la camara
 		//Siguiendo al jugador
 		if (jugadorTR != null)
 		{
-			Vector3 posicionDeseada = jugadorTR.position + offset;
+			Vector3 posicionDeseada = new Vector3(0, jugadorTR.position.y,-6) + offset;
 			Vector3 posicionDeseadaMejorada = Vector3.Lerp(transform.position, posicionDeseada, _velocidadCamara);
 			transform.position = posicionDeseadaMejorada;
 		}
-    }
+	}
 
 	#endregion
+
 
 	void conseguirJugadorTR()
 	{
