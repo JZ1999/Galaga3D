4d89d60000b13abd84bd9e992f445fc8f30653e6
Joseph Zamora <joseph.zamora64@yahoo.com>
2018-07-29 21:13:50 -0600

    Agregar sonidos a todos los botones

---
+++ Assets/Scripts/Scripts de Objetos en juego/Misc/BotonPresionado.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class BotonPresionado : MonoBehaviour {
+
+	#region Variables
+	[SerializeField]
+	private AudioSource clickSND;
+    #endregion
+
+    #region Metodos de Unity
+    
+    #endregion
+
+	public void click()
+	{
+		clickSND.Play();
+	}
+}
